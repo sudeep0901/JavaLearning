927bd3b0d10e1724cbfbbdd985fbe2dc7d13fc9a
Sudeep Patel <sudeep.tech.patel@gmail.com>
2019-12-12 19:44:55 +0530

    Spring security working

---
+++ spring-cloud-config-server/spring-clould-config-server/src/test/java/com/sudeep/springclouldconfigserver/SpringClouldConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudeep.springclouldconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClouldConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
